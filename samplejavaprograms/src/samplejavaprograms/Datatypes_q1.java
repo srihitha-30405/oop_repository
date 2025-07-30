281571eb53d84fc866079742c10bb3ab47a5348c
srihitha-30405 <2420030405@klh.edu.in>
2025-07-30 13:52:03 +0530

    Add files via upload

---
+++ samplejavaprograms/src/samplejavaprograms/Datatypes_q1.java
@@ -0,0 +1,42 @@
+package samplejavaprograms;
+
+public class Datatypes_q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    byte b=100;
+    System.out.println("byte value: "+b);
+    System.out.println("size of bite: "+ Byte.SIZE+ "bits");
+    
+    short s=20000;
+    System.out.println("Size value: "+s);
+    System.out.println("Size of short: " +Short.SIZE+ "bits");
+    
+    int i=26690;
+    System.out.println("int value: "+i);
+    System.out.println("Size of int: "+Integer.SIZE+ "bits");
+    
+    long l= 987654321;
+    System.out.println("long value: "+l);
+    System.out.println("Size of long: "+Long.SIZE+"bits" );
+    
+    float f=34.457f;
+    System.out.println("float value: "+f);
+    System.out.println("Size of float: "+Float.SIZE+"bits");
+    
+    double d=664.7555;
+    System.out.println("double value: "+d);
+    System.out.println("Size of double: "+Double.SIZE+"bits");
+    
+    char c='A';
+    System.out.println("char value: "+c);
+    System.out.println("Size of char: "+Character.SIZE+"bits");
+    
+    boolean bo=true;
+    System.out.println("boolean value: "+bo);
+    System.out.println("Size of boolean cannot be defined by java");
+    
+    
+	}
+
+}

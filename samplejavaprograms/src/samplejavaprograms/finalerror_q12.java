4744a7290fa2103eddc418eb66e326642251636f
srihitha-30405 <2420030405@klh.edu.in>
2025-08-19 00:35:11 +0530

    Add files via upload

---
+++ samplejavaprograms/src/samplejavaprograms/finalerror_q12.java
@@ -0,0 +1,11 @@
+package samplejavaprograms;
+
+public class finalerror_q12 {
+    public static void main(String[] args) {
+        final int number = 100;
+
+        System.out.println("Initial value: " + number);
+
+        System.out.println("Final value (unchanged): " + number);
+    }
+}
